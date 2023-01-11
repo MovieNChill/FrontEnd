cf027db22a98631b72d12e6a299daeee81327028
tixy74 <timlab74@gmail.com>
2023-01-11 10:19:46 +0100

    back to normal

--- src/constants/routes.tsx
+++ src/constants/routes.tsx
@@ -1,10 +1,10 @@
 import {
   Home as HomeIcon,
-  Layout,
   Messages,
   PlaylistAdd,
   Video,
 } from 'tabler-icons-react';
+import Layout from '../components/Layout';
 import Home from '../pages/Home';
 import Login from '../pages/Login';
 import MediaInfos from '../pages/MediaInfos';
